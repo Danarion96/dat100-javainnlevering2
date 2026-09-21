00734578409557dcdd9ed60ea7fbc4f32e3cd896
agentORW <oskar.wisnes@gmail.com>
2026-09-21 17:46:17 +0200

    Implement matrix methods in Matriser
    Alle oppgaver bortsett fra den om matriser. Tror jeg må implementere 3 for loops

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -36,41 +36,88 @@ public class Matriser {
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String result = "";
+
+		for (int[] table : matrise) {
+
+			for (int i = 0; i < table.length; i++) {
+
+				result += table[i];
+				if (i != table.length - 1) {
+					result += " ";
+				}
+
+			}
+
+			result += "\n";
+		}
+
+		return result;
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+		int[][] result = new int[matrise.length][matrise[0].length];
+
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[0].length; j++) {
+				result[i][j] = matrise[i][j]*tall;
+			}
+		}
+
+		return result;
 	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+		if (a.length != b.length || a[0].length != b[0].length) {
+			return false;
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[0].length; j++) {
+				if (a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 		
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
-		// TODO
+		int[][] result = new int[matrise.length][matrise[0].length];
 
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+		for (int i = 0; i < matrise.length; i++) {
+			for (int j = 0; j < matrise[0].length; j++) {
+				result[i][j] = matrise[j][i];
+			}
+		}
+
+		return result;
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
+		int[][] result = new int[a.length][b[0].length];
+
+		for (int i = 0; i < a.length; i++) {
+			int sum = 0;
+			for (int j = 0; j < b[0].length; j++) {
+				sum += (a[i][j] * b[j][i]);
+			}
+			result[i][i] = sum;
+		}
+
+		return result;
 	
 	}
 }
